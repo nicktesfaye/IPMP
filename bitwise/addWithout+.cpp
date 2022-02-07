fcb56ac10bc255ce9a26e26a5083931fc614574e
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2022-02-07 16:14:56 +0530

    Create addWithout+.cpp

---
+++ bitwise/addWithout+.cpp
@@ -0,0 +1,21 @@
+// C++ Program to add two numbers
+// without using arithmetic operator
+#include <bits/stdc++.h>
+using namespace std;
+
+int Add(int x, int y)
+{
+	if (y == 0)
+		return x;
+	else
+		return Add( x ^ y,(unsigned) (x & y) << 1);
+}
+
+// Driver code
+int main()
+{
+	cout << Add(15, 32);
+	return 0;
+}
+
+// This code is contributed by rathbhupendra
