e15d6024ca57b7213ac7aae5bff427ee0099428f
nicktesfaye <83650404+nicktesfaye@users.noreply.github.com>
2022-02-24 09:45:00 +0530

    Create intersectionOfYLinkedList.cpp

---
+++ linkedList/intersectionOfYLinkedList.cpp
@@ -0,0 +1,119 @@
+#include<iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n1,n2,n3;
+
+    cin>>T;
+    while(T--)
+    {
+        cin>>n1>>n2>>n3;
+        
+        Node* head1 = inputList(n1);
+        Node* head2 = inputList(n2);
+        Node* common = inputList(n3);
+        
+        Node* temp = head1;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        temp = head2;
+        while(temp!=NULL && temp->next != NULL)
+            temp = temp->next;
+        if(temp!=NULL) temp->next = common;
+        
+        cout << intersectPoint(head1, head2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+//Function to find intersection point in Y shaped Linked Lists.
+
+Node* level(Node* head,int d){
+    for(int i=0;i<d;i++)
+        head=head->next;
+    return head;
+}
+
+
+int intersectPoint(Node* head1, Node* head2)
+{
+   int s1=0,s2=0;
+   Node*ptr1=head1,*ptr2=head2;
+   while(ptr1){
+       s1++;
+       ptr1=ptr1->next;
+   }
+   
+    while(ptr2){
+       s2++;
+       ptr2=ptr2->next;
+   }
+   if(s1>s2){
+        ptr1=level(head1,s1-s2);
+        ptr2=head2;
+   }
+   else{
+        ptr2=level(head2,s2-s1);
+        ptr1=head1;
+   }
+   while(ptr1 != ptr2){
+       ptr1=ptr1->next;
+       ptr2=ptr2->next;
+   }
+   return ptr1->data;
+}
+
+
